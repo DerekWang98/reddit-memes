3c5ef562d768decabffd15244581c829b0524817
DerekWang98 <derekwhw@gmail.com>
2022-12-29 20:30:03 +1100

    Change browser tab title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import * as React from "react";
+
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import createEmotionCache from "../src/createEmotionCache";
+import theme from "../src/theme";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -18,20 +20,21 @@ interface MyAppProps extends AppProps {
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>Reddit Memes</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </QueryClientProvider>
   );
 }
